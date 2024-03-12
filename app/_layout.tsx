286a5ffdb0097090b61feb87490a85a987ba6857
saeid tavazani <saeidtavazani@gmail.com>
2024-03-12 11:31:14 -0700

    fix font

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,34 @@
 import { Stack } from "expo-router";
+import { useCallback, useLayoutEffect, useState } from "react";
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+
+SplashScreen.preventAutoHideAsync();
+
 const Layout = () => {
+  const [fontsLoaded] = useFonts({
+    DMBold: require("../assets/fonts/DMSans-Bold.ttf"),
+    DMMedium: require("../assets/fonts/DMSans-Medium.ttf"),
+    DMRegular: require("../assets/fonts/DMSans-Regular.ttf"),
+  });
+
+  const [layoutReady, setLayoutReady] = useState(false);
+
+  useLayoutEffect(() => {
+    const onLayoutRootView = async () => {
+      if (fontsLoaded) {
+        await SplashScreen.hideAsync();
+        setLayoutReady(true);
+      }
+    };
+
+    if (!layoutReady) {
+      onLayoutRootView();
+    }
+  }, [fontsLoaded, layoutReady]);
+
+  if (!fontsLoaded || !layoutReady) return null;
+
   return <Stack />;
 };
 
